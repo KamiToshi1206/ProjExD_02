9d590f0d22d190d4657458221ff40ad1a7ec9c43
c0b2204443 <c0b2204443@edu.teu.ac.jp>
2023-04-25 14:51:09 +0900

    練習４

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,12 @@
+import random # ランダムをインポート
+import sys 
 import pygame as pg
-import sys
-import random
+delta = {
+        pg.K_UP:(0,-1),
+        pg.K_DOWN:(0,+1),
+        pg.K_LEFT:(-1,0),
+        pg.K_RIGHT:(+1,0)
+        }
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -9,28 +15,42 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900 ,400
+
     tmr = 0
     bb_img = pg.Surface((20,20))
-    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
-    bb_img.set_colorkey((0,0,0))
-    bom = bb_img.get_rect(center=(random.randint(160,1420),random.randint(90,790)))
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10) # 爆弾を設定
+    bb_img.set_colorkey((0,0,0)) # 背景を透過させる 
+    x, y = random.randint(0,1600), random.randint(0,900)
     vx = 0
     vy = 0
-    
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = x, y
+
+
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return 0
 
+        key_lst = pg.key.get_pressed()
+        for k, mv in delta.items():
+            if key_lst[k]:
+                kk_rct.move_ip(mv)
+
+        
+
+
         tmr += 1
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
-        screen.blit(bb_img,bom)
-        bom = bb_img.get_rect(center=(vx,vy))
-        vx += 1
-        vy += 1
+        screen.blit(kk_img, kk_rct)
+        screen.blit(bb_img, bb_rct)
+        bb_rct.move_ip(vx, vy)
+        vx, vy = +1, +1
 
+        
         pg.display.update()
         clock.tick(1000)
 
