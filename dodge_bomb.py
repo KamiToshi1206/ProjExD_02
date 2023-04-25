2fefe09285c7d7b7dbab9e74db86379545a21f52
c0b2204443 <c0b2204443@edu.teu.ac.jp>
2023-04-25 09:27:26 +0900

    初期状態

---
+++ dodge_bomb.py
@@ -0,0 +1,35 @@
+import pygame as pg
+import sys
+
+def main():
+    pg.display.set_caption("逃げろ！こうかとん")
+    screen = pg.display.set_mode((1600, 900))
+    clock = pg.time.Clock()
+    bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
+    kk_img = pg.image.load("ex02/fig/3.png")
+    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    tmr = 0
+    bb_img = pg.Surface((20,20))
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
+    bb_img.set_colorkey((0,0,0))
+
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: 
+                return 0
+
+        tmr += 1
+        screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img, [900, 400])
+        screen.blit(bb_img,[160,90])
+        pg.display.update()
+        clock.tick(1000)
+
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
